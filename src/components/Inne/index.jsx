59408f776e5352294fb70f3a44b0ca60ced8de9c
Wojciech Przybyła <contact@wpww.pl>
2023-03-21 15:21:20 +0100

    A few additions to my hobbies

--- src/components/Inne/index.jsx
+++ src/components/Inne/index.jsx
@@ -4,6 +4,20 @@ import { Section } from "../Sections";
 import "./style.css";
 import FAIcon from "../FAIcon";
 import { LangContext } from "../../App";
+import { TextBox } from "../TextBox";
+import { SeeAlso, ClickTile } from "../ClickTiles";
+
+const things_i_do = [
+    {
+        code: "languages",
+        icon: "language",
+    },
+    {
+        code: "drawing",
+        icon: "pen-ruler",
+    },
+    //TODO więcej pomysłów, co robię
+];
 
 export function Inne(){
     const {setPage} = useContext(PageContext);
@@ -11,7 +25,26 @@ export function Inne(){
 
     return <Section clickTileFun={() => setPage("Intro")}>
         <h2><FAIcon icon="bicycle" /> {__("alt.headings.whatelse")}</h2>
-        <p>🚧 TBA 🚧</p>
+        <div className="grid-3">
+        {things_i_do.map(bit => 
+            <TextBox>
+                <div className="flex-right center large">
+                    <FAIcon icon={bit.icon} />
+                </div>
+                <h2>{__(`alt.whatelse.${bit.code}.label`)}</h2>
+                <p>{__(`alt.whatelse.${bit.code}.desc`)}</p>
+            </TextBox>
+        )}
+        </div>
+        
+        <SeeAlso>
+        {[
+            // ["!tarot", "https://www.github.com/wpwwhimself/"], //TODO ikona i link do tarota
+        ].map(([icon, link]) => <ClickTile
+            icon={icon} small={true} key={icon}
+            clickfun={() => window.location.assign(link)} />
+        )}
+        </SeeAlso>
         
         <h2><FAIcon icon="tv" /> {__("alt.headings.favmedia")}</h2>
         <p>🚧 TBA 🚧</p>
